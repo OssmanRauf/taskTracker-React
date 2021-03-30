d7e84b19230763a2aab4f4a206c94f419f0101e0
ossman rauf <ossmanrauf8@gmail.com>
2021-03-30 14:03:51 +0200

    error fixing

--- src/components/App.js
+++ src/components/App.js
@@ -4,50 +4,44 @@ import Tasks from "./Tasks";
 import Model from "./Model";
 
 function App() {
-    const [tasks, setTasks] = useState([]);
-    const [modalShow, setModalShow] = useState(false);
+	const [tasks, setTasks] = useState([]);
+	const [modalShow, setModalShow] = useState(false);
 
-    const deleteTask = (id) => setTasks(tasks.filter((task) => task.id !== id));
-    const bellEvent = (id) => {
-        setTasks(
-            tasks.map((task) =>
-                task.id === id ? {...task, reminder: !task.reminder } : task
-            )
-        );
-        console.log(tasks);
-    };
-    const popupModel = () => {
-        console.log("click");
-        setModalShow(true);
-        console.log(modalShow);
-    };
-    const addTask = (task) => {
-        const id = Math.floor(Math.random() * 10000) + 1;
-        const newTask = { id, ...task };
-        setTasks([...tasks, newTask]);
-        setModalShow(false);
-    };
-    return ( <
-        div className = "conteiner" >
-        <
-        Header btnClick = { popupModel }
-        /> {
-            tasks.length > 0 ? ( <
-                Tasks onBell = { bellEvent }
-                onDelete = { deleteTask }
-                tasks = { tasks }
-                />
-            ) : (
-                "No Tasks"
-            )
-        } <
-        Model onAdd = { addTask }
-        modalShow = { modalShow }
-        onHide = {
-            () => setModalShow(false) }
-        /> <
-        /div>
-    );
+	const deleteTask = (id) => setTasks(tasks.filter((task) => task.id !== id));
+	const bellEvent = (id) => {
+		setTasks(
+			tasks.map((task) =>
+				task.id === id ? { ...task, reminder: !task.reminder } : task
+			)
+		);
+		console.log(tasks);
+	};
+	const popupModel = () => {
+		console.log("click");
+		setModalShow(true);
+		console.log(modalShow);
+	};
+	const addTask = (task) => {
+		const id = Math.floor(Math.random() * 10000) + 1;
+		const newTask = { id, ...task };
+		setTasks([...tasks, newTask]);
+		setModalShow(false);
+	};
+	return (
+		<div className="conteiner">
+			<Header btnClick={popupModel} />{" "}
+			{tasks.length > 0 ? (
+				<Tasks onBell={bellEvent} onDelete={deleteTask} tasks={tasks} />
+			) : (
+				"No Tasks"
+			)}{" "}
+			<Model
+				onAdd={addTask}
+				modalShow={modalShow}
+				onHide={() => setModalShow(false)}
+			/>{" "}
+		</div>
+	);
 }
 
 export default App;
